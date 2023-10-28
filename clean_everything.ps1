51be284e4b65049b15d89f47d8ec0910c133e192
fuzzy-boiii23a <145557964+fuzzy-boiii23a@users.noreply.github.com>
2023-10-28 20:08:01 +1000

    Update clean_everything.ps1

--- clean_everything.ps1
+++ clean_everything.ps1
@@ -1,3 +1,20 @@
+$progressPreference = 'silentlyContinue'
+
+# enable darkmode
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 0
+
+# install winget https://learn.microsoft.com/en-us/windows/package-manager/winget/
+if (!(Get-Command "winget" -errorAction SilentlyContinue))
+{
+  Write-Information "Downloading WinGet and its dependencies..."
+  Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+  Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
+  Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.7.3/Microsoft.UI.Xaml.2.7.x64.appx -OutFile Microsoft.UI.Xaml.2.7.x64.appx
+  Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
+  Add-AppxPackage Microsoft.UI.Xaml.2.7.x64.appx
+  Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+}
+
 # disable UAC
 Set-ItemProperty -Path REGISTRY::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System -Name ConsentPromptBehaviorAdmin -Value 0
 
