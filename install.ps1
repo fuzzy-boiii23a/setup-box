a5d105a38ba2b605df98b35ed096cf82e9291140
fuzzy-boiii23a <145557964+fuzzy-boiii23a@users.noreply.github.com>
2023-10-28 18:01:38 +1000

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,15 +1,20 @@
+# enable darkmode
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 0
+
 # remove telemetry, disable services, and bloat
 Start-Process -FilePath powershell.exe -ArgumentList {"-ExecutionPolicy Bypass -File clean_everything.ps1"} -verb RunAs
 
-# https://learn.microsoft.com/en-us/windows/package-manager/winget/
-$progressPreference = 'silentlyContinue'
-Write-Information "Downloading WinGet and its dependencies..."
-Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
-Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
-Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.7.3/Microsoft.UI.Xaml.2.7.x64.appx -OutFile Microsoft.UI.Xaml.2.7.x64.appx
-Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
-Add-AppxPackage Microsoft.UI.Xaml.2.7.x64.appx
-Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+# install winget https://learn.microsoft.com/en-us/windows/package-manager/winget/
+ if (!(Get-Command "winget" -errorAction SilentlyContinue)){
+{
+  Write-Information "Downloading WinGet and its dependencies..."
+  Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+  Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
+  Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.7.3/Microsoft.UI.Xaml.2.7.x64.appx -OutFile Microsoft.UI.Xaml.2.7.x64.appx
+  Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
+  Add-AppxPackage Microsoft.UI.Xaml.2.7.x64.appx
+  Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+}
 
 # install VS with SDK and WDK
 winget install --source winget --exact --id Microsoft.VisualStudio.2022.Community --accept-source-agreements --override "--wait --quiet --includeRecommended --add ProductLang En-us --config $pwd\BuildTools.vsconfig"
@@ -29,6 +34,7 @@ winget install -e --id Git.Git --accept-source-agreements
 winget install GnuWin32.Make --accept-source-agreements
 
 # install llvm/clang
+Write-Information "You will be prompted during installation of LLVM, ensure you add LLVM to the PATH for the current user when prompted"
 winget install -e -i --id LLVM.LLVM --accept-source-agreements
 
 # install ripgrep
@@ -51,8 +57,7 @@ winget install --id x64dbg.x64dbg  -e --accept-source-agreements
 
 # install rust
 curl.exe https://static.rust-lang.org/rustup/dist/x86_64-pc-windows-msvc/rustup-init.exe -o rustup-init.exe
-.\rustup-init.exe
-Remove-Item rustup-init.exe -Force
+.\rustup-init.exe -y
 
 # refresh PATH so git, clang, and rust can be used
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
@@ -62,13 +67,11 @@ pip install requests pefile fastapi uvicorn
 
 # install lazyvim
 git clone https://github.com/LazyVim/starter $env:LOCALAPPDATA\nvim
-Remove-Item $env:LOCALAPPDATA\nvim\.git -Recurse -Force
 
 # install cygwin with autoconf, bash, and make
 curl.exe https://cygwin.com/setup-x86_64.exe -o cygwin-setup.exe
 .\cygwin-setup.exe --quiet-mode --no-desktop --download --local-install --no-verify -s https://mirror.aarnet.edu.au/pub/sourceware/cygwin/ -l $pwd -R C:/cygwin64
 .\cygwin-setup.exe -q -d -D -L -X -s https://mirror.aarnet.edu.au/pub/sourceware/cygwin/ -l $pwd -R C:/cygwin64 -P autoconf,make
-Remove-Item cygwin-setup.exe -Force
 
 # enable hyper-v and turn UAC back on
 Start-Process -FilePath powershell.exe -ArgumentList {"Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All -NoRestart;Set-ItemProperty -Path REGISTRY::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System -Name ConsentPromptBehaviorAdmin -Value 1"} -verb RunAs
